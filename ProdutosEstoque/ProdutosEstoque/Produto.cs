72581bce7d28b4679140a1ba0246391c3299819d
LucasDelcico <lukas-del@hotmail.com>
2023-10-06 12:18:44 -0300

    Fazendo um mini projeto de produtos para relembrar o uso de classes e atribultos em OO

---
+++ ProdutosEstoque/ProdutosEstoque/Produto.cs
@@ -0,0 +1,22 @@
+﻿
+namespace ProdutosEstoque
+{
+    public  class Produto
+    {
+       public string? Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+        public double TotalEmEstoque()
+        {
+            double total = Preco * Quantidade;
+            return total;
+        }
+        public void AdicionarProdutos(int quantity) {
+            Quantidade = Quantidade + quantity;
+        }
+        public void RemoverProdutos(int quantity)
+        {
+              Quantidade = Quantidade - quantity;
+        }
+    }
+}
---
+++ ProdutosEstoque/ProdutosEstoque/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProdutosEstoque
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int novaQuantidade;
+            Produto produto = new Produto();
+
+            PerguntaProduto();
+            produto.Nome = Console.ReadLine();
+            produto.Preco = double.Parse(Console.ReadLine());
+            produto.Quantidade = int.Parse(Console.ReadLine());
+            MostraDadosProduto(produto);
+            AdcProduto();
+            novaQuantidade= int.Parse(Console.ReadLine());
+            produto.AdicionarProdutos(novaQuantidade);
+            MostraDadosProdutoAtualizados(produto);
+            RemoverProduto();
+            novaQuantidade= int.Parse(Console.ReadLine());
+            produto.RemoverProdutos(novaQuantidade);
+            MostraDadosProdutoAtualizados(produto);
+        }
+
+        static void PerguntaProduto()
+        {
+            Console.WriteLine("Entre os dados do Produto, Nome , Preço e quantidade em estoque.");
+        }
+        static void MostraDadosProduto(Produto produto)
+        {
+            Console.WriteLine($"Dados do produto: {produto.Nome}, ${produto.Preco}, {produto.Quantidade} unidades, Total : {produto.TotalEmEstoque().ToString("F2",CultureInfo.InvariantCulture)}");
+        }
+        static void AdcProduto()
+        {
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque:");
+        }
+        static void RemoverProduto()
+        {
+            Console.Write("Digite o número de produtos a ser removido do estoque:");
+        }
+        static void MostraDadosProdutoAtualizados(Produto produto)
+        {
+        
+            Console.WriteLine($"Dados atualizados: {produto.Nome}, ${produto.Preco}, {produto.Quantidade} unidades, Total : {produto.TotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+       
+    }
+}
