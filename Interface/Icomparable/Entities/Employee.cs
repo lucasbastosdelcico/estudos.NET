f43f76e9d538e4bcca28bbad6a37fcc660090b8d
lucasbastosdelcico <lukas-del@hotmail.com>
2024-05-15 20:34:42 -0300

    Fazendo um programinha com a função Icomparable

---
+++ Interface/Icomparable/Entities/Employee.cs
@@ -0,0 +1,33 @@
+﻿namespace Icomparable.Entities
+{
+    public class Employee : IComparable
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string csvEmployee) {
+            string[] strings = csvEmployee.Split(',');
+            Name = strings[0];
+            Salary = double.Parse(strings[1]);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Salary}";
+        }
+        int  IComparable.CompareTo(object? obj)
+        {
+            if (!(obj is Employee)) {
+
+                 throw new ArgumentException("Comparing error : argument is not an Employee");
+            }
+              
+            Employee other = (Employee)obj;
+
+            return Name.CompareTo(other.Name);
+                
+
+            
+        }
+    }
+}
---
+++ Interface/Icomparable/Program.cs
@@ -0,0 +1,30 @@
+﻿using Icomparable.Entities;
+using System;
+
+namespace Icomparable { 
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\GitHub\estudos.NET\Interface\Icomparable\txt.txt";
+
+            try {
+                using (StreamReader sr = File.OpenText(path)) { 
+
+                    List<Employee> list = new List<Employee>();
+
+                    while (!sr.EndOfStream) { 
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+
+                    list.Sort();
+                    foreach (Employee e in list) { Console.WriteLine(e);}
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
